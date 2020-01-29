69818bfa74fcfde2a855a5b057d964fbcbf2aa3a
takuma <takuma.y0524@gmail.com>
2020-01-29 15:15:33 +0900

    Fix: light_emitter

--- firmware/src/light_emitter.hpp
+++ firmware/src/light_emitter.hpp
@@ -12,29 +12,17 @@ namespace gb7
     {
         Led pin;
         uint32_t sended_data = 0;
-        int timer_id = 0;
+        
+        uint32_t mask = 0;
+        uint32_t timer_id = 0;
 
-        static inline constexpr uint8_t prologue = 0b1011101;
+        static inline constexpr uint8_t prologue = 0b1011001;
         static inline constexpr uint8_t epilogue = 0b1001001;
 
     public:
         light_emitter()
         {
-            gb7::timer::multitimer::init();
-
-            using namespace gb7::timer::literals;
-            timer_id = gb7::timer::multitimer::invoke_every(500_us, 0_ms, +[](void* le) {
-                Led pin;
-                static uint32_t mask = 0;
-
-                if (mask == 0)
-                {
-                    mask = 1UL << 31;
-                }
-
-                pin = (static_cast<light_emitter<Led>*>(le)->sended_data & mask) != 0;
-                mask >>= 1;
-            }, this);
+            gb7::timer::multitimer::init();;
         }
         ~light_emitter()
         {
@@ -44,18 +32,32 @@ namespace gb7
         void set_sended_data(uint8_t data)
         {
             const uint16_t packet = 0b1010101010101010 |
-                ((sended_data & (1 << 0)) << 0) |
-                ((sended_data & (1 << 1)) << 1) |
-                ((sended_data & (1 << 2)) << 2) |
-                ((sended_data & (1 << 3)) << 3) |
-                ((sended_data & (1 << 4)) << 4) |
-                ((sended_data & (1 << 5)) << 5) |
-                ((sended_data & (1 << 6)) << 6) |
-                ((sended_data & (1 << 7)) << 7);
+                ((data & (1 << 0)) << 0) |
+                ((data & (1 << 1)) << 1) |
+                ((data & (1 << 2)) << 2) |
+                ((data & (1 << 3)) << 3) |
+                ((data & (1 << 4)) << 4) |
+                ((data & (1 << 5)) << 5) |
+                ((data & (1 << 6)) << 6) |
+                ((data & (1 << 7)) << 7);
             sended_data =
                 (static_cast<uint32_t>(prologue) << 24) |
                 (static_cast<uint32_t>(packet)   << 8 ) |
                 (static_cast<uint32_t>(epilogue) << 0 );
+
+            using namespace gb7::timer::literals;
+            gb7::timer::multitimer::cancel_invocation(timer_id);
+            timer_id = gb7::timer::multitimer::invoke_every(1_ms, 0, +[](void* le) {
+                Led pin;
+
+                if (static_cast<decltype(this)>(le)->mask == 0)
+                {
+                    static_cast<decltype(this)>(le)->mask = (1UL << 31);
+                }
+
+                pin = (static_cast<decltype(this)>(le)->sended_data & static_cast<decltype(this)>(le)->mask) != 0;
+                static_cast<decltype(this)>(le)->mask >>= 1;
+            }, this);
         }
     };
 } // namespace gb7
